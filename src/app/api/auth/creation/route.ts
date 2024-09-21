4038b9806e6d183064275e1de68abd3f7dc54266
Gulshan Sankhyan <g.sankhyan1357@gmail.com>
2024-09-20 21:37:16 -0400

    Connect Supabase db using prisma ORM

---
+++ src/app/api/auth/creation/route.ts
@@ -0,0 +1,35 @@
+import prisma from '@/app/utils/db';
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
+import { NextResponse } from 'next/server';
+
+// To store the new user data to Supabase db
+export async function GET() {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+
+  // eslint-disable-next-line @typescript-eslint/prefer-optional-chain
+  if (!user || user === null || !user.id) {
+    throw new Error('Something went wrong');
+  }
+
+  let dbUser = await prisma.user.findUnique({
+    where: {
+      id: user.id,
+    },
+  });
+
+  if (!dbUser) {
+    dbUser = await prisma.user.create({
+      data: {
+        id: user.id,
+        firstName: user.given_name ?? '',
+        lastName: user.family_name ?? '',
+        email: user.email ?? '',
+        profileImage:
+          user.picture ?? `https://avatar.vercel.sh/${user.given_name}`,
+      },
+    });
+  }
+
+  return NextResponse.redirect('http://localhost:3000/inventory');
+}
--- src/app/components/inventory/InventoryComponents.tsx
+++ src/app/components/inventory/InventoryComponents.tsx
@@ -15,8 +15,8 @@ export function InventoryComponents() {
           className={cn(
             pathname == item.href
               ? 'bg-muted text-primary'
-              : 'text-muted-foreground bg-none',
-            'hover:text-primary/70 flex items-center gap-3 rounded-lg px-3 py-2 transition-all',
+              : 'bg-none text-muted-foreground',
+            'flex items-center gap-3 rounded-lg px-3 py-2 transition-all hover:text-primary/70',
           )}
         >
           <item.icon className='size-4' />
