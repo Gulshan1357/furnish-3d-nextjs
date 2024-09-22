52d1373184d4d84738839ccd399ae152396c04ae
Gulshan Sankhyan <g.sankhyan1357@gmail.com>
2024-09-22 09:44:20 -0400

    Create a navigation component

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,86 @@
+import React from 'react';
+import Link from 'next/link';
+import { CircleUser, Store } from 'lucide-react';
+import { ThemeToggle } from '../components/ThemeToggle';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+import { Button } from '@/components/ui/button';
+import {
+  RegisterLink,
+  LoginLink,
+  LogoutLink,
+} from '@kinde-oss/kinde-auth-nextjs/components';
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
+
+interface Props {}
+
+export default async function Header({}: Props) {
+  const { getUser } = getKindeServerSession();
+  const session = await getUser();
+
+  return (
+    <header className='flex'>
+      <div className='grow-0'>
+        <Link href='/'>
+          <h1>Furnish3D</h1>
+        </Link>
+      </div>
+
+      <div className='flex grow flex-row-reverse'>
+        {session ? (
+          <div className='flex'>
+            <div>
+              <Link
+                href='/inventory'
+                className='flex items-center gap-3 rounded-lg px-3 py-2 transition-all hover:text-primary/70'
+              >
+                <Store className='size-4' />
+                Inventory
+              </Link>
+            </div>
+
+            <ThemeToggle />
+
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Button
+                  variant='secondary'
+                  size='icon'
+                  className='rounded-full'
+                >
+                  <CircleUser className='h-5 w-5' />
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent align='end'>
+                <DropdownMenuItem className='flex'>
+                  <Link className='grow' href='/inventory/cart'>
+                    Cart
+                  </Link>
+                </DropdownMenuItem>
+                <DropdownMenuItem className='flex'>
+                  <LogoutLink className='grow'>Logout</LogoutLink>
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </div>
+        ) : (
+          <div className='flex'>
+            <div>
+              <ThemeToggle />
+            </div>
+            <RegisterLink>
+              <Button>Register</Button>
+            </RegisterLink>
+            <LoginLink>
+              <Button>Login</Button>
+            </LoginLink>
+          </div>
+        )}
+      </div>
+    </header>
+  );
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import localFont from 'next/font/local';
 import './globals.css';
 import { ThemeProvider } from './components/ThemeProvider';
+import Header from './components/Header';
 
 const alpino = localFont({
   src: '../../public/fonts/Alpino-Variable.woff2',
@@ -29,6 +30,7 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
+          <Header />
           {children}
         </ThemeProvider>
       </body>
