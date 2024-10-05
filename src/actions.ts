3aa9108d800dab48a92d1a8da2010c040163d442
Gulshan Sankhyan <g.sankhyan1357@gmail.com>
2024-10-05 16:13:30 -0400

    Format with prettier

--- src/actions.ts
+++ src/actions.ts
@@ -1,66 +1,65 @@
 'use server';
 
-import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
-import { redirect } from "next/navigation";
-import prisma from "@/app/utils/db";
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
+import { redirect } from 'next/navigation';
+import prisma from '@/app/utils/db';
 
 export async function CreateSavedItemAction(formData: FormData) {
-    const { getUser } = getKindeServerSession()
+  const { getUser } = getKindeServerSession();
 
-    const user = await getUser();
+  const user = await getUser();
 
-    if (!user) {
-        return redirect('api/auth/login');
-    }
+  if (!user) {
+    return redirect('api/auth/login');
+  }
 
-    const response = await prisma.savedItem.create({
-        data: {
-            owner: {
-                connect: { id: user.id }
-            },
-            name: formData.get("name") as string,
-            modifier: formData.get("modifier") as string,
-            quantity: Number(formData.get('quantity')),
+  const response = await prisma.savedItem.create({
+    data: {
+      owner: {
+        connect: { id: user.id },
+      },
+      name: formData.get('name') as string,
+      modifier: formData.get('modifier') as string,
+      quantity: Number(formData.get('quantity')),
 
-            // Todo: add local path of image
-        },
-    });
+      // Todo: add local path of image
+    },
+  });
 
-    return redirect('/inventory');
+  return redirect('/inventory');
 }
 
 export async function DeleteSavedItem(formData: FormData) {
-    const { getUser } = getKindeServerSession()
+  const { getUser } = getKindeServerSession();
 
-    const user = await getUser();
+  const user = await getUser();
 
-    if (!user) {
-        return redirect('api/auth/login');
-    }
+  if (!user) {
+    return redirect('api/auth/login');
+  }
 
-    const data = await prisma.savedItem.delete({
-        where: {
-            ownerId: user.id,
-            id: formData.get("savedItemId") as string
-        }
-    })
-    return redirect('/cart');
+  const data = await prisma.savedItem.delete({
+    where: {
+      ownerId: user.id,
+      id: formData.get('savedItemId') as string,
+    },
+  });
+  return redirect('/cart');
 }
 
 export async function DeleteAllSavedItem(formData: FormData) {
-    const { getUser } = getKindeServerSession()
+  const { getUser } = getKindeServerSession();
 
-    const user = await getUser();
+  const user = await getUser();
 
-    if (!user) {
-        return redirect('api/auth/login');
-    }
+  if (!user) {
+    return redirect('api/auth/login');
+  }
 
-    const data = await prisma.savedItem.deleteMany({
-        where: {
-            ownerId: user.id,
-        }
-    })
-    return redirect('/cart');
-
+  const data = await prisma.savedItem.deleteMany({
+    where: {
+      ownerId: user.id,
+    },
+  });
+  return redirect('/cart');
 }
