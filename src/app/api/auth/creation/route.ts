37be7a6381862c5b3009d1803d1a86e16d478974
Gulshan Sankhyan <g.sankhyan1357@gmail.com>
2024-10-05 18:31:13 -0400

    fix 4

--- src/app/api/auth/creation/route.ts
+++ src/app/api/auth/creation/route.ts
@@ -4,36 +4,44 @@ import { NextResponse } from 'next/server';
 
 // To store the new user data to Supabase db
 export async function GET() {
-  const { getUser } = getKindeServerSession();
-  const user = await getUser();
+  try {
+    const { getUser } = getKindeServerSession();
+    const user = await getUser();
 
-  // eslint-disable-next-line @typescript-eslint/prefer-optional-chain
-  if (!user || user === null || !user.id) {
-    throw new Error('Something went wrong');
-  }
+    // eslint-disable-next-line @typescript-eslint/prefer-optional-chain
+    if (!user || user === null || !user.id) {
+      throw new Error('Something went wrong');
+    }
 
-  let dbUser = await prisma.user.findUnique({
-    where: {
-      id: user.id,
-    },
-  });
-
-  if (!dbUser) {
-    dbUser = await prisma.user.create({
-      data: {
+    let dbUser = await prisma.user.findUnique({
+      where: {
         id: user.id,
-        firstName: user.given_name ?? '',
-        lastName: user.family_name ?? '',
-        email: user.email ?? '',
-        profileImage:
-          user.picture ?? `https://avatar.vercel.sh/${user.given_name}`,
       },
     });
+
+    if (!dbUser) {
+      dbUser = await prisma.user.create({
+        data: {
+          id: user.id,
+          firstName: user.given_name ?? '',
+          lastName: user.family_name ?? '',
+          email: user.email ?? '',
+          profileImage:
+            user.picture ?? `https://avatar.vercel.sh/${user.given_name}`,
+        },
+      });
+    }
+
+    // local
+    // return NextResponse.redirect('http://localhost:3000/inventory');
+
+    // remote
+    return NextResponse.redirect('https://furnish-3d.vercel.app/inventory');
+  } catch (error) {
+    console.error('Error in /api/auth/creation:', error);
+    return NextResponse.json(
+      { error: 'Internal Server Error' },
+      { status: 500 },
+    );
   }
-
-  // local
-  // return NextResponse.redirect('http://localhost:3000/inventory');
-
-  // remote
-  return NextResponse.redirect('https://furnish-3d.vercel.app/inventory');
 }
