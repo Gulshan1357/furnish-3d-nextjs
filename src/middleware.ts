26c4771072f8ee319e700ce772bb9fb08ff73131
Gulshan Sankhyan <g.sankhyan1357@gmail.com>
2024-10-05 18:00:41 -0400

    Vercel deployment fix try

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,16 +1,23 @@
 import { withAuth } from '@kinde-oss/kinde-auth-nextjs/middleware';
 
-export const config = {
-  matcher: [
-    /*
-     * Match all request paths except for the ones starting with:
-     * - api (API routes)
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
-     * - favicon.ico, sitemap.xml, robots.txt (metadata files)
-     */
-    '/((?!api|about|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt|$).*)',
-  ],
-};
+// export const config = {
+//   matcher: [
+//     /*
+//      * Match all request paths except for the ones starting with:
+//      * - api (API routes)
+//      * - _next/static (static files)
+//      * - _next/image (image optimization files)
+//      * - favicon.ico, sitemap.xml, robots.txt (metadata files)
+//      */
+//     '/((?!api|about|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt|$).*)',
+//   ],
+// };
 
-export default withAuth;
+// export default withAuth;
+
+export default function middleware(req) {
+  return withAuth(req);
+}
+export const config = {
+  matcher: ["/inventory", "/cart"]
+};
