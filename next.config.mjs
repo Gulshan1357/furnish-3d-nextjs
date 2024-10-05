9b74216207ec3a70fb270e26e9338fcf8f294f03
Gulshan Sankhyan <g.sankhyan1357@gmail.com>
2024-10-05 18:14:05 -0400

    Rollback next.config.mjs and fix 2

--- next.config.mjs
+++ next.config.mjs
@@ -3,16 +3,6 @@ const nextConfig = {
   // Enable server-side rendering by default
   reactStrictMode: true,
 
-  // Explicitly handle the exportPathMap to ensure API routes are excluded from static export
-  async exportPathMap() {
-    return {
-      // Define your static pages here
-      '/': { page: '/' },
-      // Other static pages you may have
-      // '/about': { page: '/about' },
-    };
-  },
-
   // Disable static generation for API routes
   generateBuildId: async () => {
     return 'build-id';
