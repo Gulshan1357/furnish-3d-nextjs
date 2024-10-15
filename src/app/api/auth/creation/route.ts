9f0e85b81ce442cfbc1cd199c7c7b582f60ec869
Gulshan Sankhyan <g.sankhyan1357@gmail.com>
2024-10-15 11:24:25 -0400

    prettier

--- src/app/api/auth/creation/route.ts
+++ src/app/api/auth/creation/route.ts
@@ -1,4 +1,4 @@
-export const dynamic = "force-dynamic";
+export const dynamic = 'force-dynamic';
 
 import prisma from '@/app/utils/db';
 import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
